3cd2c3cf8075b28a9ac33b0f50077bfd6493ea0f
ajeeshram12 <111558702+ajeeshram12@users.noreply.github.com>
2022-09-26 13:50:23 +0530

    Update Python_For Caps.py

--- Python_For Caps.py
+++ Python_For Caps.py
@@ -1,20 +1,3 @@
-Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-names=["csdm-WI29-6201","csdm-WI29-6202","csdm-WA20-6201","csdm-WA20-6202","csdm-UP50-6201","csdm-UP50-6202","csdm-SUPX-6201","csdm-SUPX-6202","csdm-TA00-6201","csdm-TA00-6202","csdm-OAON-6201","csdm-OAON-6202","csdm-PH17-6201","csdm-PH17-6202","csdm-RO06-6201","csdm-RO06-6202","csdm-TH30-6201","csdm-TH30-6202","csdm-TE50-6201","csdm-TE50-6202","csdm-RI33-6201","csdm-RI33-6202","csdm-RE69-6201","csdm-RE69-6202","csdm-SE03-6201","csdm-SE03-6202","csdm-WE06-6201","csdm-WE06-6202","csdm-OMON-6201","csdm-OMON-6202","csdm-RA37-6201","csdm-RA37-6202","csdm-WD10-6201","csdm-WD10-6202","csdm-NO44-6201","csdm-NO44-6202","csdm-NJL2-6201","csdm-NJL2-6202","csdm-NE2X-6201","csdm-NE2X-6202","csdm-NA80-6201","csdm-NA80-6202","csdm-MU94-6201","csdm-MU94-6202","csdm-MI17-6201","csdm-MI17-6202","csdm-MO40-6201","csdm-MO40-6202"]
-names= [x.lower() for x in names]
-print(*names, sep = '\n')
-
-SyntaxError: multiple statements found while compiling a single statement
-names=["csdm-WI29-6201","csdm-WI29-6202","csdm-WA20-6201","csdm-WA20-6202","csdm-UP50-6201","csdm-UP50-6202","csdm-SUPX-6201","csdm-SUPX-6202","csdm-TA00-6201","csdm-TA00-6202","csdm-OAON-6201","csdm-OAON-6202","csdm-PH17-6201","csdm-PH17-6202","csdm-RO06-6201","csdm-RO06-6202","csdm-TH30-6201","csdm-TH30-6202","csdm-TE50-6201","csdm-TE50-6202","csdm-RI33-6201","csdm-RI33-6202","csdm-RE69-6201","csdm-RE69-6202","csdm-SE03-6201","csdm-SE03-6202","csdm-WE06-6201","csdm-WE06-6202","csdm-OMON-6201","csdm-OMON-6202","csdm-RA37-6201","csdm-RA37-6202","csdm-WD10-6201","csdm-WD10-6202","csdm-NO44-6201","csdm-NO44-6202","csdm-NJL2-6201","csdm-NJL2-6202","csdm-NE2X-6201","csdm-NE2X-6202","csdm-NA80-6201","csdm-NA80-6202","csdm-MU94-6201","csdm-MU94-6202","csdm-MI17-6201","csdm-MI17-6202","csdm-MO40-6201","csdm-MO40-6202"]
-names= [x.lower() for x in names]
-print(*names, sep = '\n')
-SyntaxError: multiple statements found while compiling a single statement
-names=["csdm-WI29-6201","csdm-WI29-6202","csdm-WA20-6201","csdm-WA20-6202","csdm-UP50-6201","csdm-UP50-6202","csdm-SUPX-6201","csdm-SUPX-6202","csdm-TA00-6201","csdm-TA00-6202","csdm-OAON-6201","csdm-OAON-6202","csdm-PH17-6201","csdm-PH17-6202","csdm-RO06-6201","csdm-RO06-6202","csdm-TH30-6201","csdm-TH30-6202","csdm-TE50-6201","csdm-TE50-6202","csdm-RI33-6201","csdm-RI33-6202","csdm-RE69-6201","csdm-RE69-6202","csdm-SE03-6201","csdm-SE03-6202","csdm-WE06-6201","csdm-WE06-6202","csdm-OMON-6201","csdm-OMON-6202","csdm-RA37-6201","csdm-RA37-6202","csdm-WD10-6201","csdm-WD10-6202","csdm-NO44-6201","csdm-NO44-6202","csdm-NJL2-6201","csdm-NJL2-6202","csdm-NE2X-6201","csdm-NE2X-6202","csdm-NA80-6201","csdm-NA80-6202","csdm-MU94-6201","csdm-MU94-6202","csdm-MI17-6201","csdm-MI17-6202","csdm-MO40-6201","csdm-MO40-6202"]
-names= [x.lower() for x in names]
-print(*names, sep = '\n')
-
-SyntaxError: multiple statements found while compiling a single statement
-
 names=["csdm-WI29-6201","csdm-WI29-6202","csdm-WA20-6201","csdm-WA20-6202","csdm-UP50-6201","csdm-UP50-6202","csdm-SUPX-6201","csdm-SUPX-6202","csdm-TA00-6201","csdm-TA00-6202","csdm-OAON-6201","csdm-OAON-6202","csdm-PH17-6201","csdm-PH17-6202","csdm-RO06-6201","csdm-RO06-6202","csdm-TH30-6201","csdm-TH30-6202","csdm-TE50-6201","csdm-TE50-6202","csdm-RI33-6201","csdm-RI33-6202","csdm-RE69-6201","csdm-RE69-6202","csdm-SE03-6201","csdm-SE03-6202","csdm-WE06-6201","csdm-WE06-6202","csdm-OMON-6201","csdm-OMON-6202","csdm-RA37-6201","csdm-RA37-6202","csdm-WD10-6201","csdm-WD10-6202","csdm-NO44-6201","csdm-NO44-6202","csdm-NJL2-6201","csdm-NJL2-6202","csdm-NE2X-6201","csdm-NE2X-6202","csdm-NA80-6201","csdm-NA80-6202","csdm-MU94-6201","csdm-MU94-6202","csdm-MI17-6201","csdm-MI17-6202","csdm-MO40-6201","csdm-MO40-6202"]
 names= [x.lower() for x in names]
 print(*names, sep = '\n')
