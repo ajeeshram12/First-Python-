94a2b4a3f078d7315280f61868d81372d915bac3
ajeeshram12 <111558702+ajeeshram12@users.noreply.github.com>
2022-09-26 13:49:33 +0530

    Add files via upload

---
+++ Python_For Caps.py
@@ -0,0 +1,68 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+names=["csdm-WI29-6201","csdm-WI29-6202","csdm-WA20-6201","csdm-WA20-6202","csdm-UP50-6201","csdm-UP50-6202","csdm-SUPX-6201","csdm-SUPX-6202","csdm-TA00-6201","csdm-TA00-6202","csdm-OAON-6201","csdm-OAON-6202","csdm-PH17-6201","csdm-PH17-6202","csdm-RO06-6201","csdm-RO06-6202","csdm-TH30-6201","csdm-TH30-6202","csdm-TE50-6201","csdm-TE50-6202","csdm-RI33-6201","csdm-RI33-6202","csdm-RE69-6201","csdm-RE69-6202","csdm-SE03-6201","csdm-SE03-6202","csdm-WE06-6201","csdm-WE06-6202","csdm-OMON-6201","csdm-OMON-6202","csdm-RA37-6201","csdm-RA37-6202","csdm-WD10-6201","csdm-WD10-6202","csdm-NO44-6201","csdm-NO44-6202","csdm-NJL2-6201","csdm-NJL2-6202","csdm-NE2X-6201","csdm-NE2X-6202","csdm-NA80-6201","csdm-NA80-6202","csdm-MU94-6201","csdm-MU94-6202","csdm-MI17-6201","csdm-MI17-6202","csdm-MO40-6201","csdm-MO40-6202"]
+names= [x.lower() for x in names]
+print(*names, sep = '\n')
+
+SyntaxError: multiple statements found while compiling a single statement
+names=["csdm-WI29-6201","csdm-WI29-6202","csdm-WA20-6201","csdm-WA20-6202","csdm-UP50-6201","csdm-UP50-6202","csdm-SUPX-6201","csdm-SUPX-6202","csdm-TA00-6201","csdm-TA00-6202","csdm-OAON-6201","csdm-OAON-6202","csdm-PH17-6201","csdm-PH17-6202","csdm-RO06-6201","csdm-RO06-6202","csdm-TH30-6201","csdm-TH30-6202","csdm-TE50-6201","csdm-TE50-6202","csdm-RI33-6201","csdm-RI33-6202","csdm-RE69-6201","csdm-RE69-6202","csdm-SE03-6201","csdm-SE03-6202","csdm-WE06-6201","csdm-WE06-6202","csdm-OMON-6201","csdm-OMON-6202","csdm-RA37-6201","csdm-RA37-6202","csdm-WD10-6201","csdm-WD10-6202","csdm-NO44-6201","csdm-NO44-6202","csdm-NJL2-6201","csdm-NJL2-6202","csdm-NE2X-6201","csdm-NE2X-6202","csdm-NA80-6201","csdm-NA80-6202","csdm-MU94-6201","csdm-MU94-6202","csdm-MI17-6201","csdm-MI17-6202","csdm-MO40-6201","csdm-MO40-6202"]
+names= [x.lower() for x in names]
+print(*names, sep = '\n')
+SyntaxError: multiple statements found while compiling a single statement
+names=["csdm-WI29-6201","csdm-WI29-6202","csdm-WA20-6201","csdm-WA20-6202","csdm-UP50-6201","csdm-UP50-6202","csdm-SUPX-6201","csdm-SUPX-6202","csdm-TA00-6201","csdm-TA00-6202","csdm-OAON-6201","csdm-OAON-6202","csdm-PH17-6201","csdm-PH17-6202","csdm-RO06-6201","csdm-RO06-6202","csdm-TH30-6201","csdm-TH30-6202","csdm-TE50-6201","csdm-TE50-6202","csdm-RI33-6201","csdm-RI33-6202","csdm-RE69-6201","csdm-RE69-6202","csdm-SE03-6201","csdm-SE03-6202","csdm-WE06-6201","csdm-WE06-6202","csdm-OMON-6201","csdm-OMON-6202","csdm-RA37-6201","csdm-RA37-6202","csdm-WD10-6201","csdm-WD10-6202","csdm-NO44-6201","csdm-NO44-6202","csdm-NJL2-6201","csdm-NJL2-6202","csdm-NE2X-6201","csdm-NE2X-6202","csdm-NA80-6201","csdm-NA80-6202","csdm-MU94-6201","csdm-MU94-6202","csdm-MI17-6201","csdm-MI17-6202","csdm-MO40-6201","csdm-MO40-6202"]
+names= [x.lower() for x in names]
+print(*names, sep = '\n')
+
+SyntaxError: multiple statements found while compiling a single statement
+
+names=["csdm-WI29-6201","csdm-WI29-6202","csdm-WA20-6201","csdm-WA20-6202","csdm-UP50-6201","csdm-UP50-6202","csdm-SUPX-6201","csdm-SUPX-6202","csdm-TA00-6201","csdm-TA00-6202","csdm-OAON-6201","csdm-OAON-6202","csdm-PH17-6201","csdm-PH17-6202","csdm-RO06-6201","csdm-RO06-6202","csdm-TH30-6201","csdm-TH30-6202","csdm-TE50-6201","csdm-TE50-6202","csdm-RI33-6201","csdm-RI33-6202","csdm-RE69-6201","csdm-RE69-6202","csdm-SE03-6201","csdm-SE03-6202","csdm-WE06-6201","csdm-WE06-6202","csdm-OMON-6201","csdm-OMON-6202","csdm-RA37-6201","csdm-RA37-6202","csdm-WD10-6201","csdm-WD10-6202","csdm-NO44-6201","csdm-NO44-6202","csdm-NJL2-6201","csdm-NJL2-6202","csdm-NE2X-6201","csdm-NE2X-6202","csdm-NA80-6201","csdm-NA80-6202","csdm-MU94-6201","csdm-MU94-6202","csdm-MI17-6201","csdm-MI17-6202","csdm-MO40-6201","csdm-MO40-6202"]
+names= [x.lower() for x in names]
+print(*names, sep = '\n')
+csdm-wi29-6201
+csdm-wi29-6202
+csdm-wa20-6201
+csdm-wa20-6202
+csdm-up50-6201
+csdm-up50-6202
+csdm-supx-6201
+csdm-supx-6202
+csdm-ta00-6201
+csdm-ta00-6202
+csdm-oaon-6201
+csdm-oaon-6202
+csdm-ph17-6201
+csdm-ph17-6202
+csdm-ro06-6201
+csdm-ro06-6202
+csdm-th30-6201
+csdm-th30-6202
+csdm-te50-6201
+csdm-te50-6202
+csdm-ri33-6201
+csdm-ri33-6202
+csdm-re69-6201
+csdm-re69-6202
+csdm-se03-6201
+csdm-se03-6202
+csdm-we06-6201
+csdm-we06-6202
+csdm-omon-6201
+csdm-omon-6202
+csdm-ra37-6201
+csdm-ra37-6202
+csdm-wd10-6201
+csdm-wd10-6202
+csdm-no44-6201
+csdm-no44-6202
+csdm-njl2-6201
+csdm-njl2-6202
+csdm-ne2x-6201
+csdm-ne2x-6202
+csdm-na80-6201
+csdm-na80-6202
+csdm-mu94-6201
+csdm-mu94-6202
+csdm-mi17-6201
+csdm-mi17-6202
+csdm-mo40-6201
+csdm-mo40-6202
